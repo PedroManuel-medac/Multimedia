c8a80ed692497e08e5d5422e00720bc2b166a298
PedroManuel-medac <plb0008@alu.medac.es>
2025-10-07 22:02:46 +0200

    Inicio de sesión evaluable

---
+++ Ejercicio_Evaluable/app/src/main/java/com/example/ejercicio_evaluable/Inicio.java
@@ -0,0 +1,16 @@
+package com.example.ejercicio_evaluable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+
+    }
+}
