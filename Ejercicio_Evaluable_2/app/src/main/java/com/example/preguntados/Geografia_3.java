f99512fdfaffd33d62e6576d3fbc57cbf1971de9
PedroManuel-medac <plb0008@alu.medac.es>
2025-11-04 22:28:33 +0100

    ejercicio evaluable 2

---
+++ Ejercicio_Evaluable_2/app/src/main/java/com/example/preguntados/Geografia_3.java
@@ -0,0 +1,14 @@
+package com.example.preguntados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Geografia_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geografia3);
+    }
+}
---
+++ Ejercicio_Evaluable_2/app/src/main/java/com/example/preguntados/Geografia_5.java
@@ -0,0 +1,14 @@
+package com.example.preguntados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Geografia_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geografia5);
+    }
+}
